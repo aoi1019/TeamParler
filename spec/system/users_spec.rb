216522b5e4e3f27d677778de8275fc36652b42fe
Ryuya Takusari <takuryu.1049@icloud.com>
2020-12-11 16:38:15 +0900

    結合テスト実装 - ユーザー登録・ログイン機能

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  context 'ユーザー登録機能' do
+    it '正しい値を入力した場合⇢ 登録に成功し、トップページに遷移する' do
+      # 入力の為のダミーデータ生成
+      @user = FactoryBot.build(:user)
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+      # トップページに遷移
+      visit root_path
+
+      # ユーザー登録ページへ遷移
+      visit new_user_registration_path
+
+      # 登録の為の情報を入力(正しい値)
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
+
+      # 登録ボタンをクリック
+      find('input[name="commit"]').click
+
+      # トップページに遷移していることを確認
+      #root_pathは仮置しています😨
+      expect(current_path).to eq root_path
+
+    end
+    it '不正な値を入力した場合⇢ 登録に失敗し、ユーザー登録ページに戻る' do
+      # 入力の為のダミーデータ生成
+      @user = FactoryBot.build(:user)
+
+      # トップページに遷移
+      visit root_path
+
+      # ユーザー登録ページへ遷移
+      visit new_user_registration_path
+
+      # 登録の為の情報を入力(正しい値)
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      # 故意にhogeと入力する(不正な値)
+      fill_in 'user_password_confirmation', with: "hoge"
+
+      # 登録ボタンをクリック
+      find('input[name="commit"]').click
+
+      # トップページに遷移していることを確認
+      expect(current_path).to eq "/users"
+    end
+  end
+  context 'ログイン機能' do
+    it '正しい値を入力した場合⇢ ログインに成功し、トップページに遷移する' do
+
+      # 予め、ユーザーをDBに保存する
+      @user = FactoryBot.create(:user)
+
+      # トップページに遷移
+      visit root_path
+
+      # ログインページへ遷移
+      visit new_user_session_path
+      
+      # ログインの為の情報を入力(正しい値)
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+
+      # ログインボタンをクリック
+      find('input[name="commit"]').click
+
+      # トップページに遷移していることを確認する
+      #root_pathは仮置しています😨
+      expect(current_path).to eq root_path
+
+    end
+    it '不正な値を入力した場合⇢ ログインに失敗し、再びログインページに戻ってくる' do
+
+      # 予め、ユーザーをDBに保存する
+      @user = FactoryBot.create(:user)
+
+      # トップページに遷移
+      visit new_user_session_path
+
+      # ログインページへ遷移
+      visit new_user_session_path
+
+      # ログインの為の情報を入力(不正な値)
+      fill_in 'user_email', with: "hoge"
+      fill_in 'user_password', with: "hoge"
+
+      # ログインボタンをクリック
+      find('input[name="commit"]').click
+
+      # ログインページに戻ってきていることを確認する
+      expect(current_path).to eq new_user_session_path
+
+    end
+  end
 end
